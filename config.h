cb9335d1d454a86fbc6d35a3aed8dc1a74766f70
Raaed Shaikh <sraaed1908@gmail.com>
2020-06-02 23:22:57 +0530

    clean up keybindings

--- config.h
+++ config.h
@@ -1,30 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
-        "Terminus:size=12",
+	"xos4 Terminus:size=12",
         "FontAwesome:size=14",
         "JoyPixels:pixelsize=10:antialias=true:autohint=true",
 };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
+/* static const char col_gray1[]       = "#344654"; */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#1D2021";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray3  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }; */
+static const char *tags[] = { "eins", "zwei", "drei", "vier" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -41,7 +43,7 @@ static const Rule rules[] = {
 	{ "St",       "st",       "calcurse",   0,            1,             1,           -1 },
 	{ "Display",  "display",  NULL,         0,            1,             1,           -1 },
 	{ "Pcmanfm",  "pcmanfm",  NULL,         0,            1,             1,           -1 },
-	{ "mpv",      NULL,       NULL,         1 << 6,       1,             1,           -1 },
+	{ "mpv",      NULL,       NULL,         0,            0,             1,           -1 },
 };
 
 /* layout(s) */
@@ -98,12 +100,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	TAGKEYS(                        XK_0,                      9)
+	/* TAGKEYS(                        XK_5,                      4) */
+	/* TAGKEYS(                        XK_6,                      5) */
+	/* TAGKEYS(                        XK_7,                      6) */
+	/* TAGKEYS(                        XK_8,                      7) */
+	/* TAGKEYS(                        XK_9,                      8) */
+	TAGKEYS(                        XK_0,                      3)
 };
 
 /* button definitions */
@@ -122,4 +124,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
