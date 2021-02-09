8f29b085c8ea696ccb195bb39db56843457e0889
Raaed Shaikh <raaed@tuta.io>
2021-02-09 21:59:25 +0530

    yes

--- config.def.h
+++ config.def.h
@@ -1,22 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
+        /* "Iosevka:pixelsize=15:antialias=true:autohint=true", */
         "Terminus (TTF):pixelsize=16:antialias=true:autohint=true",
+        /* "Fantasque Sans Mono:pixelsize=16:antialias=true:autohint=true", */
         "FontAwesome:size=10",
         "JoyPixels:pixelsize=10:antialias=true:autohint=true",
 };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_border[]      = "#00AABB";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#ffffff";
+static const char col_border[]      = "#FF0038";
+/* static const char col_gray4[]       = "#eeeeee"; */
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
